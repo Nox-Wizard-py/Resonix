c1340de3d87414317a1a0debd5fa6c4f8f201a10
Nox Wizard <hossainazmul132@gmail.com>
2026-01-20 19:02:35 +0530

    Bump version to v1.5.1 wave I

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
     applicationId = "com.noxwizard.resonix"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.5.1"
 
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
