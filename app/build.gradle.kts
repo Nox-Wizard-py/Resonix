b7fea577a3d216c6d403136a08c6d4328368fc20
Nox Wizard <hossainazmul132@gmail.com>
2026-02-23 13:55:57 +0530

    Chore: Bump version to v1.6.0 and fix PipePipeExtractor JitPack dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
     applicationId = "com.noxwizard.resonix"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.5.1"
+        versionCode = 3
+        versionName = "1.6.0"
         
         manifestPlaceholders["appAuthRedirectScheme"] = "com.noxwizard.resonix"
 
