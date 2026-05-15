d77c60287736d9912757b4b1dd64b8b83fa8d29c
Nox Wizard <hossainazmul132@gmail.com>
2026-05-15 13:35:25 +0530

    feat(settings): add SliderEmojiKey for custom slider knob personalization

--- app/src/main/kotlin/com/noxwizard/resonix/constants/PreferenceKeys.kt
+++ app/src/main/kotlin/com/noxwizard/resonix/constants/PreferenceKeys.kt
@@ -9,6 +9,7 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 
 val DynamicThemeKey = booleanPreferencesKey("dynamicTheme")
+val SliderEmojiKey = stringPreferencesKey("sliderEmoji")
 val DarkModeKey = stringPreferencesKey("darkMode")
 val PureBlackKey = booleanPreferencesKey("pureBlack")
 val DefaultOpenTabKey = stringPreferencesKey("defaultOpenTab")
